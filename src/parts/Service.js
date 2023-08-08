e6764ded095c844bf4fce0cc698eb2fc94c9d358
abubakar7879 <93710180+abubakar7879@users.noreply.github.com>
2023-08-08 21:51:52 +0500

    Change in HERO

--- src/parts/Service.js
+++ src/parts/Service.js
@@ -18,17 +18,17 @@ export default function Service({ data }) {
 
         <div className="grid grid-rows-3 px-10 gap-8 sm:grid-cols-3 sm:grid-rows-1 sm:gap-6 xl:gap-16">
           {
-                        data.map((item, index) => (
-                          <Fade bottom delay={500 * index}>
-                            <div>
-                              <div className="bg-white group rounded-2xl shadow-2xl border border-light-theme-purple transform transition duration-500 hover:scale-105">
-                                <img src={item.imageUrl} alt="Service" className="w-full rounded-t-2xl" />
-                                <h2 className="text-theme-blue text-center text-xl py-7 rounded-b-2xl">{item.title}</h2>
-                              </div>
-                            </div>
-                          </Fade>
-                        ))
-                    }
+            data.map((item, index) => (
+              <Fade bottom delay={500 * index}>
+                <div>
+                  <div className="bg-white group rounded-2xl shadow-2xl border border-light-theme-purple transform transition duration-500 hover:scale-105">
+                    <img src={item.imageUrl} alt="Service" className="w-full rounded-t-2xl" />
+                    <h2 className="text-theme-blue text-center text-xl py-7 rounded-b-2xl">{item.title}</h2>
+                  </div>
+                </div>
+              </Fade>
+            ))
+          }
         </div>
       </div>
     </div>
